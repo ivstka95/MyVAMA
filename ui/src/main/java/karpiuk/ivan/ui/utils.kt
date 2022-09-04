7bbaeed7696c52982f3ec5a0dba5872141457d36
Ivan Karpiuk <ivstka95@gmail.com>
2022-09-05 01:01:30 +0300

    created util function to format date

---
+++ ui/src/main/java/karpiuk/ivan/ui/utils.kt
@@ -0,0 +1,26 @@
+package karpiuk.ivan.ui
+
+import android.content.Context
+
+fun String.formattedDate(context: Context): String {
+    val dateComponents = split('-')
+    val year = dateComponents[0]
+    val month =
+        when (dateComponents[1]) {
+            "01" -> context.getString(R.string.month_january)
+            "02" -> context.getString(R.string.month_february)
+            "03" -> context.getString(R.string.month_march)
+            "04" -> context.getString(R.string.month_april)
+            "05" -> context.getString(R.string.month_may)
+            "06" -> context.getString(R.string.month_june)
+            "07" -> context.getString(R.string.month_july)
+            "08" -> context.getString(R.string.month_august)
+            "09" -> context.getString(R.string.month_september)
+            "10" -> context.getString(R.string.month_october)
+            "11" -> context.getString(R.string.month_november)
+            "12" -> context.getString(R.string.month_december)
+            else -> dateComponents[1]
+        }
+    val day = dateComponents[2]
+    return "$month $day, $year"
+}
